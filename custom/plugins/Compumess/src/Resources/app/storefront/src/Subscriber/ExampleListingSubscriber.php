b516dc2fd7f73e98540a060facb9c57d07998889
playerinvn <mr.tranmanh90@gmail.com>
2023-07-25 12:23:24 +0700

    Category

---
+++ custom/plugins/Compumess/src/Resources/app/storefront/src/Subscriber/ExampleListingSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+class ExampleListingSubscriber implements EventSubscriberInterface
+{
+    // register event
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProductListingCollectFilterEvent::class => 'addFilter'
+        ];
+    }
+
+    public function addFilter(ProductListingCollectFilterEvent $event): void
+    {
+        // fetch existing filters
+        $filters = $event->getFilters();
+        $request = $event->getRequest();
+
+        $filtered = (bool) $request->get('migration_ComuMess5_product_series');
+
+        $filter = new Filter(
+            // unique name of the filter
+            'migration_ComuMess5_product_series',
+
+            // defines if this filter is active
+            $filtered,
+
+            // Defines aggregations behind a filter. A filter can contain multiple aggregations like properties
+            [
+                new FilterAggregation(
+                    'active-filter',
+                    new MaxAggregation('active', 'product.customFields.migration_ComuMess5_product_series'),
+                    [new EqualsFilter('product.customFields.migration_ComuMess5_product_series', true)]
+                ),
+            ],
+
+
+            // defines the DAL filter which should be added to the criteria   
+            new EqualsFilter('product.customFields.migration_ComuMess5_product_series', true),
+
+            // defines the values which will be added as currentFilter to the result
+            $filtered
+        );
+
+        // Add your custom filter
+        $filters->add($filter);
+    }
+}
---
+++ custom/plugins/Compumess/src/Resources/app/storefront/src/js/main.js
@@ -0,0 +1,12 @@
+
+function filterBtnCustom() {
+    var filterTriggerBtn = document.querySelector('.filter-panel-wrapper .action--filter-btn');
+    var filterPanelItemsContainer = document.querySelector('.filter-panel-wrapper .filter-panel .filter-panel-items-container');
+
+    filterTriggerBtn.addEventListener('click', function () {
+        filterTriggerBtn.classList.toggle('filter-actived');
+        filterPanelItemsContainer.classList.toggle('active');
+    });
+}
+filterBtnCustom();
+
