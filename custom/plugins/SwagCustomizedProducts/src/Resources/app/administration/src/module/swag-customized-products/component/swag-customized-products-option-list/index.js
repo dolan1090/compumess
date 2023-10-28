f219bcc1f410430a80b57981455053973e481582
Tran Manh <77145935+dolan1090@users.noreply.github.com>
2023-10-28 20:52:55 +0700

    Revert "Plugin-Custom-Products"

--- custom/plugins/SwagCustomizedProducts/src/Resources/app/administration/src/module/swag-customized-products/component/swag-customized-products-option-list/index.js
+++
@@ -1,8 +0,0 @@
-import template from './swag-customized-products-option-list.html.twig';
-import './swag-customized-products-option-list.scss';
-
-const { Component } = Shopware;
-
-Component.extend('swag-customized-products-option-list', 'sw-one-to-many-grid', {
-    template,
-});
--- custom/plugins/SwagCustomizedProducts/src/Resources/app/storefront/src/swag-customized-products-exclusion-list-validation/option-types/exclusion-list-type-htmleditor.js
+++
@@ -1,8 +0,0 @@
-export default {
-    type: ['htmleditor'],
-
-    validate: ({ element, operator }) => {
-        const plugin = window.PluginManager.getPluginInstanceFromElement(element, 'SwagCustomizedProductsHtmlEditor');
-        return (operator === 'X' ? !plugin.isEmpty() : plugin.isEmpty());
-    },
-};
