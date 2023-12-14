9a1a949fc6125b58a89e064ba344a81561be401c
playerinvn <mr.tranmanh90@gmail.com>
2023-12-14 17:56:44 +0700

    update feedback 14/12

--- custom/plugins/Compumess/src/Resources/app/storefront/src/js/main.js
+++ custom/plugins/Compumess/src/Resources/app/storefront/src/js/main.js
@@ -12,3 +12,17 @@ function filterBtnCustom() {
 }
 filterBtnCustom();
 
+function manufacture_h() {
+    var manufacture_h_items = document.querySelectorAll('.manufacture-h .row .col-md-4');
+
+    if (manufacture_h_items) {
+        for (let i = 0; i < manufacture_h_items.length; i++) {
+            var manufacture_h_element_text = manufacture_h_items[i].querySelector('.cms-element-text');
+
+            if (manufacture_h_element_text.textContent.trim() === '') {
+                manufacture_h_items[i].style.display = "none";
+            }
+        }
+    }
+}
+manufacture_h();
