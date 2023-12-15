3d7829e1d4552b3d5dbd477f952702effe4cfa3e
playerinvn <mr.tranmanh90@gmail.com>
2023-12-15 17:19:48 +0700

    Revert "update price Inquiry"
    This reverts commit 9f857aa731604f7a997c148118c813c48728db1d.

--- custom/plugins/Compumess/src/Resources/app/storefront/src/js/main.js
+++ custom/plugins/Compumess/src/Resources/app/storefront/src/js/main.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    filterBtnCustom();
-    manufacture_h();
-    addToCartInquiry();
-    addToCartInquiry_category();
-});
 
-//category product
 function filterBtnCustom() {
     var filterTriggerBtn = document.querySelector('.filter-panel-wrapper .action--filter-btn');
     var filterPanelItemsContainer = document.querySelector('.filter-panel-wrapper .filter-panel .filter-panel-items-container');
@@ -17,21 +10,8 @@ function filterBtnCustom() {
         });
     }
 }
+filterBtnCustom();
 
-function addToCartInquiry_category() {
-    var inquiryPriceInfoList = document.querySelectorAll('.product-price-info');
-
-    inquiryPriceInfoList.forEach(function(inquiryPriceInfo) {
-        var inquiryPrice = inquiryPriceInfo.querySelector('.product-price-wrapper .product-price');
-
-        if (inquiryPrice && inquiryPrice.innerHTML.trim() === 'Preis auf Anfrage') {
-            inquiryPriceInfo.style.display = 'none';
-        }
-    });
-}
-
-
-//hom page
 function manufacture_h() {
     var manufacture_h_items = document.querySelectorAll('.manufacture-h .row .col-md-4');
 
@@ -45,22 +25,4 @@ function manufacture_h() {
         }
     }
 }
-
-//detail product
-function addToCartInquiry() {
-    var inquiryPrice = document.querySelector('.product-detail-content .inquiry-price');
-    var quaity = document.querySelector('.product-detail-form-container form .col-4.d-flex.justify-content-end')
-    var rowInquiryPrice = document.querySelector('.product-detail-form-container .row.g-2.buy-widget-container.mt-2');
-
-    if (inquiryPrice && inquiryPrice.innerHTML == 'Preis auf Anfrage') {
-        inquiryPrice.style.display = 'none';
-
-        if (quaity) {
-            quaity.classList.add('d-none');
-        }
-
-        if (rowInquiryPrice) {
-            rowInquiryPrice.classList.remove('mt-2');
-        }
-    }
-}
+manufacture_h();
